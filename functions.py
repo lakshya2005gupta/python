8ad57bf09c0dabef908455545510174cb528d971
lakshya2005gupta <lakshyadelhigupta@gmail.com>
2025-08-18 03:45:48 +0530

    update

--- functions.py
+++ functions.py
@@ -1,6 +1,15 @@
 #recursion 
-def sum(n):
-    if (n==1) :
-        return 1    
-    return n + sum(n-1)
-print(sum(5))
+# def sum(n):
+#     if (n==1) :
+#         return 1    
+#     return n + sum(n-1)
+# print(sum(5))
+ 
+ #using recursion to print a pattern
+# def pattern(n):
+#     if n == 0:
+#         return
+#     print("* " * n)
+#     pattern(n - 1)  
+# n = int(input("Enter the number of rows for the pattern: "))
+# pattern(n)
